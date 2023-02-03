d498279b945a519a9d58d26c17169d3e1e4ea65e
oopsmails <oopsmails@gmail.com>
2023-02-03 16:38:55 -0500

    project name revised

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title '002-oopsmails-angular-lib-a14'`, () => {
+  it(`should have as title 'angular-base'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('002-oopsmails-angular-lib-a14');
+    expect(app.title).toEqual('angular-base');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('002-oopsmails-angular-lib-a14 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'angular-base app is running!'
+    );
   });
 });
